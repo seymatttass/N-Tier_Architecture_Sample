54a86e600170f376a2de156b8a2a542b9ea9a4cf
Şeyma Taş <126778789+seymatttass@users.noreply.github.com>
2025-01-30 10:53:12 +0300

    add layer

---
+++ WebAPI/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Business.DTOs.Student;
+using Microsoft.AspNetCore.Mvc;
+using School.Business.Abstract;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent([FromBody] StudentCreateDto input)
+        {
+            var resultEntityDto = await _studentService.AddAsync(input);
+            return Ok(resultEntityDto);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateStudent(Guid id, [FromBody] StudentUpdateDto input)
+        {
+            var resultEntityDto = await _studentService.UpdateAsync(id, input);
+            return Ok(resultEntityDto);
+        }
+    }
+}
